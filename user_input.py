c21b41ea029e0ff3f6355a03156713100dc7735f
beth98an <76478762+beth98an@users.noreply.github.com>
2021-03-22 16:54:26 +0000

    user_input added

---
+++ user_input.py
@@ -0,0 +1,37 @@
+# Get user inputs
+# first name
+first_name = input("What is your first name? ")
+
+# last name
+last_name = input("What is your last name? ")
+
+# age
+age = int(input("What is your age? "))
+
+# salary
+salary = float(input("What is your average salary per hour? "))
+
+# date of birth
+date_of_birth = input("What is your date of birth? dd/mm/yyyy ")
+
+# post code
+post_code = input("What is your post code? ")
+
+# print the type of each input
+print(first_name)
+print(type(first_name))
+
+print(last_name)
+print(type(last_name))
+
+print(age)
+print(type(age))
+
+print(salary)
+print(type(salary))
+
+print(date_of_birth)
+print(type(date_of_birth))
+
+print(post_code)
+print(type(post_code))
